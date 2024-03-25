6203584e5dac4652bcf3b7ae1823760ced7a58b1
khalillovv <kapriz2k16@gmail.com>
2024-03-25 21:14:56 +0200

    f

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
-import { DB_PASSWORD } from "./secret.js";
-const password = DB_PASSWORD;
 
-const URI = `mongodb+srv://admin:${password}@cluster0.et4khhd.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0`;
+// const URI = `mongodb+srv://admin:${password}@cluster0.et4khhd.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose
   .connect(process.env.MONGODB_URI)
