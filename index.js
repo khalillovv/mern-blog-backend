0e3fe40ea417a331267d5c16fe02ccbcd2172032
khalillovv <kapriz2k16@gmail.com>
2024-03-25 21:48:50 +0200

    fix uploads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import cors from "cors";
+import fs from "fs";
 
 import {
   registerValidation,
@@ -17,7 +18,10 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads");
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
